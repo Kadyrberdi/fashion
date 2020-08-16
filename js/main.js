87bee9edc7f5f7dce0d764feed8ce79aaa09b3a0
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-08-16 11:53:11 +0300

    added send.php and jquery with validate and cllbck

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
-var reviewSlider = new Swiper(".swiper-container__reviews", {
-  spaceBetween: 30,
-  loop: true,
-  centeredSlides: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 7000,
-    disableOnInteraction: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-var storySlider = new Swiper(".story__swiper-container", {
-  loop: true,
-  navigation: {
-    nextEl: ".story__button_next",
-    prevEl: ".story__button_prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
+$(document).ready(function () {
+  var reviewSlider = new Swiper(".swiper-container__reviews", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var storySlider = new Swiper(".story__swiper-container", {
+    loop: true,
+    navigation: {
+      nextEl: ".story__button_next",
+      prevEl: ".story__button_prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
+  /*$(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        email: {
+          required: "*If you want to subscribe leave your email",
+          email: "*Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  });*/
 });
