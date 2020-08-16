ed3fcdab9e01170801530fd3356ae89b37d05fc9
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-08-17 02:59:18 +0300

    create: added modal section with styles

--- js/main.js
+++ js/main.js
@@ -24,15 +24,23 @@ $(document).ready(function () {
       onlyInViewport: true,
     },
   });
-  /*$(".form").each(function () {
-    $(this).validate({
-      errorClass: "invalid",
-      messages: {
-        email: {
-          required: "*If you want to subscribe leave your email",
-          email: "*Your email address must be in the format of name@domain.com",
-        },
-      },
-    });
-  });*/
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  function openModal() {
+    var modalDialog = $(".modal__dialog");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalDialog = $(".modal__dialog");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      var modalDialog = $(".modal__dialog");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
 });
